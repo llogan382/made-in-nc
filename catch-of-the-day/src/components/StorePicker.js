185c023c74dc096db384cf2dfe81a6337b0efc42
Luke Logan <llogan382@gmail.com>
2020-01-07 11:00:00 -0500

    End video 3

---
+++ catch-of-the-day/src/components/StorePicker.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+//every class needs at least one method, called "RENDER"
+class StorePicker extends React.Component {
+    render() {
+        return <p>Hello!</p>
+    }
+}
+
+export default StorePicker;
--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,1 +1,10 @@
-// let's go!
+import React from 'react';
+import StorePicker from './components/StorePicker';
+
+import { render } from 'react-dom';
+
+
+
+render(
+    <StorePicker />, document.querySelector('#main')
+); //render takes two items: The item to render, and the HTML element to attacch to
