ed22f2d2c43f7a468e3c44e1ece6f3bb5868bded
Luke Logan <llogan382@gmail.com>
2020-01-07 11:15:32 -0500

    Adds form tag

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 
 //every class needs at least one method, called "RENDER"
 class StorePicker extends React.Component {
     render() {
-        return <p>Hello!</p>
+        return (
+            <form className="store-selector">
+
+                <h2>Please enter a store name</h2>
+                <input type="text" required placeholder="Store Name" />
+                <button type="submit">Visit Store>></button>
+            </form>
+        )
     }
 }
 
