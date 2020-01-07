5f0a311d995397eae453d40d2d1b6068851ad126
Luke Logan <llogan382@gmail.com>
2020-01-07 11:26:11 -0500

    Adds stylesheet

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,12 +1,10 @@
-import React, { Fragment } from 'react';
-
+import React from 'react';
 
 //every class needs at least one method, called "RENDER"
 class StorePicker extends React.Component {
     render() {
         return (
             <form className="store-selector">
-
                 <h2>Please enter a store name</h2>
                 <input type="text" required placeholder="Store Name" />
                 <button type="submit">Visit Store>></button>
--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import StorePicker from './components/StorePicker';
-
 import { render } from 'react-dom';
+import StorePicker from './components/StorePicker';
+import "./css/style.css";
 
 
 
