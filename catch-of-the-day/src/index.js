aefba29b0ecf5bf57d778ac4121510c141d52e3f
Luke Logan <llogan382@gmail.com>
2020-01-07 14:17:30 -0500

    Hardcodes the layout with components

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { render } from 'react-dom';
 import StorePicker from './components/StorePicker';
+import App from './components/App';
 import "./css/style.css";
 
 
 
 render(
-    <StorePicker />, document.querySelector('#main')
+    <App />, document.querySelector('#main')
 ); //render takes two items: The item to render, and the HTML element to attacch to
