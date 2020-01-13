a6707653febc65648cb75b2216b2b93b1ad76c72
Luke Logan <llogan382@gmail.com>
2020-01-13 06:20:37 -0500

    Event handling- basic concepts

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,12 +1,30 @@
 import React from 'react';
+import { getFunName } from '../helpers';
 
 //every class needs at least one method, called "RENDER"
 class StorePicker extends React.Component {
+
+    // When button is clicked, log the info
+    handleClick = (e) => {
+        e.preventDefault();
+        console.log(e);
+    }
+
     render() {
         return (
             <form className="store-selector">
                 <h2>Please enter a store name</h2>
-                <input type="text" required placeholder="Store Name" />
+
+                {/* Button events need to be added in-line */}
+                <button onClick={this.handleClick}>Button</button>
+
+                <input type="text" required placeholder="Store Name"
+                    // Values on input need to be connected to state.
+                    defaultValue={getFunName()}
+
+
+
+                />
                 <button type="submit">Visit Store>></button>
             </form>
         )
