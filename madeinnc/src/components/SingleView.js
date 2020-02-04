3216e01333baa34807ee50613e7727233539fbe0
Luke Logan <llogan382@gmail.com>
2020-02-04 06:57:41 -0500

    single view is successful!

---
+++ madeinnc/src/components/SingleView.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+//every class needs at least one method, called "RENDER"
+class ItemView extends React.Component {
+
+
+    componentDidMount() {
+        // const { itemSlug } = this.props.details.match.params
+
+        this.setState()
+    }
+
+    filterItem = props => {
+
+        const itemslug = this.props.item.itemSlug;
+        const allItems = Object.values(this.props.details);
+        // Go through an object, filter by Store
+        // Get object WHERE object.key=== Object.value =slug
+        const currentItem = allItems.filter(item => {
+            if (item.itemSlug === itemslug) {
+                return true;
+            }
+        })
+
+
+        return currentItem;
+    };
+
+    render() {
+        const itemDetails = this.filterItem()[0]
+        console.log(itemDetails)
+        return (
+            <div>
+                <img src={itemDetails.image} alt="" />
+                <p>{itemDetails.itemName}</p>
+                <p>{itemDetails.location}</p>
+                <p>{itemDetails.madeby}</p>
+                <p>{itemDetails.category}</p>
+                <p>{itemDetails.price}</p>
+                Single View!</div>
+
+        )
+    }
+}
+
+
+export default ItemView;
