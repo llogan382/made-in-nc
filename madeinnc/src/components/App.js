3fe6fe16947ddfacd61b823d23e0348c87cccb03
Luke Logan <llogan382@gmail.com>
2020-02-04 06:36:57 -0500

    Removes unused code

--- madeinnc/src/components/App.js
+++ madeinnc/src/components/App.js
@@ -15,6 +15,7 @@ import {
   useRouteMatch,
   useParams
 } from "react-router-dom";
+import ItemView from './ItemView';
 
 
 class App extends React.Component {
@@ -29,30 +30,23 @@ class App extends React.Component {
     return (
       <Router>
         <div>
-          <ul>
-            <li>
-              <Link to="/">All Items</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/topics">Topics</Link>
-            </li>
-          </ul>
 
           <Switch>
-            <Route path="/about">
-              <About />
-            </Route>
-            <Route path="/:storeslug"
+            <Route exact path="/:storeslug"
               render={(props) => <StoreView
                 storeslug={props.match.params}
                 details={this.state.items.sampleItems} isAuthed={true} />}
             >
+            </Route>
+            <Route path="/:storeslug/:itemSlug"
+              render={(props) => <ItemView
+                item={props.match.params}
+                itemSlug={props.match.params}
+                details={this.state.items.sampleItems} isAuthed={true} />}
+            >
 
             </Route>
-            <Route path="/">
+            <Route exact path="/">
               <AllItems
                 details={this.state.items.sampleItems}
               />
@@ -67,48 +61,3 @@ class App extends React.Component {
 export default App;
 
 
-
-function About() {
-  return <h2>About</h2>;
-}
-function Home() {
-  return <h2>Home</h2>;
-}
-function Topics() {
-  let match = useRouteMatch();
-
-  return (
-    <div>
-      <h2>Topics</h2>
-
-      <ul>
-        <li>
-          <Link to={`${match.url} / components`}>Components</Link>
-        </li>
-        <li>
-          <Link to={`${match.url} / props - v - state`}>
-            Props v. State
-          </Link>
-        </li>
-      </ul>
-
-      {/* The Topics page has its own <Switch> with more routes
-          that build on the /topics URL path. You can think of the
-          2nd <Route> here as an "index" page for all topics, or
-          the page that is shown when no topic is selected */}
-      <Switch>
-        <Route path={`${match.path} /: topicId`}>
-          <Topic />
-        </Route>
-        <Route path={match.path}>
-          <h3>Please select a topic.</h3>
-        </Route>
-      </Switch>
-    </div>
-  );
-}
-
-function Topic() {
-  let { topicId } = useParams();
-  return <h3>Requested topic ID: {topicId}</h3>;
-}
