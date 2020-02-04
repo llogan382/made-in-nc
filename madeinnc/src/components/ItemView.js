0e142b153727f6fff6c199da8db292b949aa7389
Luke Logan <llogan382@gmail.com>
2020-02-04 06:57:31 -0500

    Separates single item from item view loop

--- madeinnc/src/components/ItemView.js
+++ madeinnc/src/components/ItemView.js
@@ -4,40 +4,37 @@ import { Link } from 'react-router-dom';
 //every class needs at least one method, called "RENDER"
 class ItemView extends React.Component {
 
-    filterItem = props => {
-        const itemslug = this.props.item.itemSlug;
-        const allItems = Object.values(this.props.details);
-        // Go through an object, filter by Store
-        // Get object WHERE object.key=== Object.value =slug
-        const currentItem = allItems.filter(item => {
-            if (item.itemSlug === itemslug) {
-                return true;
-            }
-        })
 
-
-        return currentItem;
-    };
     componentDidMount() {
         // const { itemSlug } = this.props.details.match.params
 
         this.setState()
     }
 
+
     render() {
-        const itemInfo = this.filterItem()[0]
+
         return (
             <div>
 
+                <Link to={`${this.props.details.storeSlug}/${this.props.details.itemSlug}`}>
+                    <img src={this.props.details.image} />
+                </Link>
+                <h2>{this.props.details.itemName}</h2>
+                <p>{this.props.details.itemName}</p>
+                <p>{this.props.details.location}</p>
+                <p>{this.props.details.madeBy}</p>
+                <p>{this.props.details.category}</p>
+                <p>{this.props.details.price}</p>
+                <p>{this.props.details.storeName}</p>
+                <Link to={`${this.props.details.storeSlug}`}>
+                    <p>{this.props.details.storeSlug}</p>
+                </Link>
 
-                <img src={itemInfo.image} alt="" />
-                <p>{itemInfo.itemName}</p>
-                <p>{itemInfo.location}</p>
-                <p>{itemInfo.madeBy}</p>
-                <p>{itemInfo.category}</p>
 
 
-                Hello!</div>
+
+                Hello!</div >
 
         )
     }
